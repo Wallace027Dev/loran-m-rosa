74d62439ebc99a59d0dec93e15ec3810eb4db057
Wallace Vieira <wallaceofc@hotmail.com>
2023-05-04 21:36:31 -0300

    feat: "putting protection on private pages"

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,20 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
-import Welcome from '../pages/Welcome';
-import Login from '../pages/Login';
-import Signup from '../pages/Signup';
-import Forgot from '../pages/Forgot';
-import Home from '../pages/Home';
+import { Routes, Route } from "react-router-dom";
+import Welcome from "../pages/Welcome";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Forgot from "../pages/Forgot";
+import Home from "../pages/Home";
+import { ProtectedRoute } from "../components/PrivateRoutes";
 
 function AppRoutes() {
-	return (
-		<Routes>
-			<Route path='/' element={ <Welcome /> } />
-			<Route path='/login' element={ <Login /> } />
-			<Route path='/signup' element={ <Signup /> } />
-			<Route path='/forgot' element={ <Forgot /> } />
-			<Route path='/home' element={ <Home /> } />
-		</Routes>
-	)
+  return (
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/forgot" element={<Forgot />} />
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default AppRoutes;
