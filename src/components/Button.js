d98e36ab549b5bad69275a2c8a8f1ee97a05cb02
Wallace Vieira <wallaceofc@hotmail.com>
2023-05-18 19:04:42 -0300

    feat: edit button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,22 +2,40 @@ import styled from 'styled-components'
 
 export default styled.button`
   cursor: pointer;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   background: var(--primary);
   border-radius: 1.25rem;
-  border: none;
+  border: 2px solid var(--primary);
   color: var(--light-font);
   font-size: 1.2rem;
   height: 4rem;
   margin-bottom: 1.5rem;
   max-width: 50rem;
   width: 100%;
+  transition: 0.2s ease-in;
 
-  display: flex;
-  align-items: center;
-  justify-content: center;
   &::placeholder {
     color: var(--tertiary);
     font-weight: bold;
   }
+
+  &:hover {
+    background: var(--light-font);
+    color: var(--primary);
+  }
+
+  &:active {
+    background: var(--secondary);
+    color: var(--primary);
+  }
+
+  &[disabled] {
+    background: var(--tertiary);
+    cursor: default;
+  }
 `
+
+//border: 2px solid var(--secondary);
