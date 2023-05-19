a7341644d20b4939d4ea96a1f88332937f5e5574
Wallace Vieira <wallaceofc@hotmail.com>
2023-05-19 19:54:09 -0300

    feat: getting the data and handling the submit event

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -6,24 +6,56 @@ import FormGroup from '../FormGroup'
 import Input from '../Input'
 import Select from '../Select'
 import Button from '../Button'
+import { useState } from 'react'
 
 export default function UserForm ({ buttonLabel }) {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [category, setCategory] = useState('')
+
+  function handleSubmit (e) {
+    e.preventDefault()
+
+    console.log({
+      name,
+      email,
+      phone,
+      category
+    })
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input placeholder='Nome' />
-      </FormGroup>
-      <FormGroup error='O formato do e-mail é inválido.'>
-        <Input placeholder='Email' error />
-      </FormGroup>
-      <FormGroup>
-        <Input placeholder='Telefone' />
+        <Input
+          value={name}
+          placeholder='Nome'
+          onChange={e => setName(e.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Input
+          value={email}
+          placeholder='Email'
+          onChange={e => setEmail(e.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          value={phone}
+          placeholder='Telefone'
+          onChange={e => setPhone(e.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select value={category} onChange={e => setCategory(e.target.value)}>
           <option value='instagram'>Instagram</option>
           <option value='Facebook'>Facebook</option>
+          <option value='Facebook e Instagram'>Facebook e Instagram</option>
         </Select>
       </FormGroup>
 
