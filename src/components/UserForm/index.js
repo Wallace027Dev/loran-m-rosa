fe3e232fa2c621f492c1c6f76975b233a82495e9
Wallace Vieira <wallaceofc@hotmail.com>
2023-05-20 09:09:22 -0300

    feat: abstracting logic using Custom Hook

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import { useState } from 'react'
 
 import isEmailValid from '../../utils/isEmailValid'
+import useErrors from '../../hooks/useErrors'
 
 import { Form } from './styles'
 
@@ -15,18 +16,15 @@ export default function UserForm ({ buttonLabel }) {
   const [email, setEmail] = useState('')
   const [phone, setPhone] = useState('')
   const [category, setCategory] = useState('')
-  const [errors, setErrors] = useState([])
+
+  const { setError, removeError, getErrorMessageByFieldName } = useErrors()
 
   function handleNameChange (e) {
     setName(e.target.value)
-    // Checks if the user has typed something in the 'name' field
     if (!e.target.value) {
-      setErrors(prevState => [
-        ...prevState,
-        { field: 'name', message: 'Nome é obrigatório.' }
-      ])
+      setError({ field: 'name', message: 'Nome é obrigatório.' })
     } else {
-      setErrors(prevState => prevState.filter(error => error.field !== 'name'))
+      removeError('name')
     }
   }
 
@@ -35,20 +33,9 @@ export default function UserForm ({ buttonLabel }) {
     // Checks if the user has typed something in the
     // 'email' field and validates using regex
     if (e.target.value && !isEmailValid(e.target.value)) {
-      // Checks if there is already an object with error
-      // 'email.field' = 'email' in the 'errors' object
-      const errorAlreadyExists = errors.find(error => error.field === 'email')
-      // if exists, execution stops
-      if (errorAlreadyExists) {
-        return
-      }
-      // otherwise, insert this object
-      setErrors(prevState => [
-        ...prevState,
-        { field: 'email', message: 'E-mail inválido.' }
-      ])
+      setError({ field: 'email', message: 'E-mail inválido.' })
     } else {
-      setErrors(prevState => prevState.filter(error => error.field !== 'email'))
+      removeError('email')
     }
   }
 
@@ -61,14 +48,6 @@ export default function UserForm ({ buttonLabel }) {
       phone,
       category
     })
-  }
-
-  function getErrorMessageByFieldName (fieldName) {
-    // look in the error array and look for an object with
-    // field equal to field using THIS function
-    return errors.find(error => error.field === fieldName)?.message
-    //the '?', checks if the value exists and is valid,
-    // and if so, returns the 'message'
   }
 
   return (
