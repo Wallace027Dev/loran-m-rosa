c9689c3f1ef7b41184fafa74686acb1836ef71cc
Wallace Vieira <wallaceofc@hotmail.com>
2023-05-30 18:40:23 -0300

    feat: replacing the CustomEvent with the EventManager

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -3,12 +3,13 @@ import { useState, useEffect } from 'react';
 
 import ToastMessage from '../ToastMessage';
 
+import { toastEventManager } from '../../../utils/toast';
+
 export default function ToastContainer() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    function handleAddToast(event) {
-      const { type, text } = event.detail;
+    function handleAddToast({ type, text }) {
 
       setMessages((prevState) => [
         ...prevState,
@@ -16,11 +17,11 @@ export default function ToastContainer() {
       ]);
     }
 
-    document.addEventListener('addtoast', handleAddToast);
+    toastEventManager.on('addtoast', handleAddToast);
 
     return () => {
-      document.removeEventListener('addtoast', handleAddToast)
-    }
+      toastEventManager.removeListener('addtoast', handleAddToast)
+    };
   }, []);
 
 
